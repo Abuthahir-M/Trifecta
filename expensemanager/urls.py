256584fbee4ef257d9c5e2dfb8e46a2077134dc0
Abuthahir <167390105+Abuthahir-M@users.noreply.github.com>
2024-05-10 19:02:59 +0530

    Delete expensemanager directory

--- expensemanager/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'expensemanager'
-urlpatterns = [
-    path('expense-home/', views.expenseHome, name='expense-home')
-]
--- expensemanager/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def expenseHome(request):
-    return render(request, 'expensemanager/expense-home.html')
