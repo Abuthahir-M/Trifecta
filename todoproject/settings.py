608b9b5386bd18e3a0e2af29daf9143dc3c9f73f
Abuthahir <167390105+Abuthahir-M@users.noreply.github.com>
2024-05-10 19:36:49 +0530

    Update settings.py

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@9^fe-&zwb&!aokb0e*bnt1nxzar*j3v-5gps6l#i0x=u9i@8l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
